89f65803e2fe1364f792bc77e2c45ab48a78612a
Johannes Bertens <johannes.bertens@gmail.com>
2017-10-22 10:45:46 +0200

    Using metamask

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import SciFiContract from '../build/contracts/SciFi.json'
 import getWeb3 from './utils/getWeb3'
 
 import './css/oswald.css'
@@ -18,31 +17,22 @@ class App extends Component {
       web3: null,
       sciFiInstance: null,
       account: null,
-      image: "images/fulls/Term.jpg",
-      selectedMovie: "Movie"
+      image: "images/fulls/terminator.jpg",
+      selectedMovie: "Terminator"
     }
-  }
-
-  componentDidMount() {
-    //mainInit.init();
   }
 
   componentWillMount() {
     // Get network provider and web3 instance.
-    // See utils/getWeb3 for more info.
+    getWeb3.then(results => {
+      this.setState({ web3: results.web3 });
 
-    getWeb3
-      .then(results => {
-        this.setState({
-          web3: results.web3
-        })
-
-        // Instantiate contract once web3 provided.
-        this.instantiateContract()
-      })
-      .catch(() => {
-        console.log('Error finding web3.')
-      })
+      // Instantiate contract once web3 provided.
+      this.instantiateContract();
+    }).catch((e) => {
+      console.log('Error finding web3.')
+      console.log(e);
+    })
   }
 
   updateValue(modifiedValue) {
@@ -52,16 +42,22 @@ class App extends Component {
   }
 
   voteFromButton = () => {
-    this.voteForMovie(this.state.toVote, () => this.updateState());
+    this.voteForMovie(this.state.toVote, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        this.updateState();
+      }
+    });
   }
 
   voteForMovie = (movie, callback) => {
     var name = this.state.web3.fromAscii(movie);
-    this.state.sciFiInstance.vote(name, { value: 200, from: this.state.account }).then(callback);
+    this.state.sciFiInstance.vote(name, { value: 200, from: this.state.account }, callback);
   }
 
   updateState = () => {
-    this.state.sciFiInstance.movie_num.call().then((res) => {
+    this.state.sciFiInstance.movie_num.call((err, res) => {
       this.setState({ movieNum: res.c[0] });
       this.setState({ movies: [] });
 
@@ -75,10 +71,10 @@ class App extends Component {
 
   setMovieDetails = (index) => {
     var movieName;
-    this.state.sciFiInstance.movies.call(index).then((res) => {
-      movieName = this.state.web3.toAscii(res).trim().replace(/[^A-Za-z 0-9 \.,\?""!@#\$%\^&\*\(\)-_=\+;:<>\/\\\|\}\{\[\]`~]*/g, '');
-      this.state.sciFiInstance.bids.call(res).then((res) => {
-        this.setState({ movies: [...this.state.movies, movieName + ' [' + res + ']'] });
+    this.state.sciFiInstance.movies.call(index, (err, name) => {
+      movieName = this.state.web3.toAscii(name).trim().replace(/[^A-Za-z 0-9 \.,\?""!@#\$%\^&\*\(\)-_=\+;:<>\/\\\|\}\{\[\]`~]*/g, '');
+      this.state.sciFiInstance.bids.call(name, (err2, bid) => {
+        this.setState({ movies: [...this.state.movies, [movieName, bid]] });
       });
     });
   }
@@ -91,30 +87,23 @@ class App extends Component {
      * state management library, but for convenience I've placed them here.
      */
 
-    const contract = require('truffle-contract')
-    const sciFi = contract(SciFiContract);
-    sciFi.setProvider(this.state.web3.currentProvider);
-
     this.state.web3.eth.getAccounts((error, accounts) => {
-      sciFi.deployed().then((instance) => {
-        this.setState({ sciFiInstance: instance });
-        this.setState({ account: accounts[0] });
-
-        this.voteForMovie('Terminator 2', () => {
-          // Get the movies from the contract
-
-          this.updateState();
-        });
-
-      });
+      const sciFi = this.state.web3.eth.contract([{ "constant": true, "inputs": [{ "name": "", "type": "uint256" }], "name": "movies", "outputs": [{ "name": "", "type": "bytes32" }], "type": "function" }, { "constant": true, "inputs": [], "name": "movie_num", "outputs": [{ "name": "", "type": "uint256" }], "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "bytes32" }], "name": "bids", "outputs": [{ "name": "", "type": "uint256" }], "type": "function" }, { "constant": false, "inputs": [{ "name": "name", "type": "bytes32" }], "name": "vote", "outputs": [], "type": "function" }]).at('0xd94badbec21695b7a36abcb979efad0108319d18');
+      this.setState({ sciFiInstance: sciFi });
+      this.setState({ account: accounts[0] });
+      this.updateState();
     });
 
   }
 
   switchTo = (e) => {
     this.setState({ selectedMovie: e.target.alt });
-    this.setState({ image: e.target.parentElement.href});
+    this.setState({ image: e.target.parentElement.href });
     e.preventDefault();
+  }
+
+  toFileName(name) {
+    return name.toLowerCase().replace(':','_');
   }
 
   render() {
@@ -123,25 +112,22 @@ class App extends Component {
         <div id="main">
 
           <header id="header">
-            <h1>Smart Movie Voting</h1>
-            <p>Pay to win!</p>
-            <p>The current list is:</p>
-
+            <h2>Smart Movie Voting</h2>
           </header>
 
           <section id="thumbnails">
-            {this.state.movies.map((name, index) => {
+            {this.state.movies.sort((a, b) => {return b[1] - a[1]}).map(([name, bid], index) => {
               return <article key={index}>
-                <a onClick={this.switchTo.bind(this)} className="thumbnail" href={"images/fulls/" + name.substring(0, 4).toLowerCase() + ".jpg"}>
-                  <img src={"images/thumbs/" + name.substring(0, 4).toLowerCase() + ".jpg"} alt={name} />
+                <a onClick={this.switchTo.bind(this)} className="thumbnail" href={"images/fulls/" + this.toFileName(name) + ".jpg"}>
+                  <img src={"images/thumbs/" + this.toFileName(name) + ".jpg"} alt={name + ' [' + bid + ']'} />
                 </a>
-                <h2>{name}</h2>
+                
               </article>;
             })}
           </section>
 
           <footer id="footer">
-            <input placeholder="New Movie" className="input" value={this.state.toVote} onChange={this.updateValue.bind(this)}></input>
+            <input placeholder="Movie" className="input" value={this.state.toVote} onChange={this.updateValue.bind(this)}></input>
             <button className="xsmall button" onClick={this.voteFromButton.bind(this)}>Vote</button>
           </footer>
 
